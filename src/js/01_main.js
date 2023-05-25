5ff6716cae6c875ed30197faa6a96eba1438e242
NikKolia2 <denis26e@gmail.com>
2023-05-25 10:18:31 +0400

    dfgdgdf

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1495,6 +1495,3 @@ class CalculatorV2{
 
 new CalculatorV2(document.querySelector(".calculator-v2--js"));
 
-document.querySelectorAll("[data-hide-text]").forEach(function(item){
-  item.innerHTML = item.dataset.hideText;
-});
